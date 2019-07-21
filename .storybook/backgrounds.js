fd99eeb03b092ce9a3c335b07f898d533f1c1418
Jon-Paul Glencross <jon-paul.glencross@uk.ibm.com>
2019-07-21 19:10:37 +0100

    Add storybook config

---
+++ .storybook/backgrounds.js
@@ -0,0 +1,20 @@
+const backgrounds = [
+  { name: "Light", value: "#FFF", default: true },
+  { name: "Dark", value: "#333" },
+  {
+    name: "Fanta funk",
+    value: "linear-gradient(110deg, #fdcd3b 60%, #ffed4b 60%);"
+  },
+  {
+    name: "Vimto funk",
+    value:
+      "linear-gradient(110deg, #a60af3 40%, rgba(0, 0, 0, 0) 30%), radial-gradient(farthest-corner at 0% 0%, #7a00cc 70%, #c03fff 70%);"
+  },
+  {
+    name: "All kinda funk",
+    value:
+      "linear-gradient(70deg, #fff810  30%, rgba(0,0,0,0) 30%), linear-gradient(30deg, #63e89e 60%, #ff7ee3 60%);"
+  }
+];
+
+export default backgrounds;
