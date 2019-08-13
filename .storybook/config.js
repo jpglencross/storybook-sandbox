aded4a45e47861b52222d9fd41d97a67e3acc01a
Jon-Paul Glencross <jon-paul.glencross@uk.ibm.com>
2019-08-13 20:14:37 +0100

    Add A11y to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,11 @@
 import { configure, addDecorator, addParameters } from "@storybook/react";
 import { themes } from "@storybook/theming";
 import { withKnobs } from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
 import backgrounds from "./backgrounds";
 
 addDecorator(withKnobs);
+addDecorator(withA11y);
 addParameters({ backgrounds: backgrounds });
 addParameters({
   options: {
